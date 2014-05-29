c0c84ce7f5bcf3eea8c5f328fae04050b53aeb1a
Joshua Marple <marpljos@gmail.com>
2014-05-28 21:17:44 -0400

    adding in obstacles, cleaned up and commented code

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
 
+
+class Obstacle(Widget):
+    name = "obstacle"
+
 class Background(Widget):
+    #needs no methods or other functions, so we just pass it
+    #has to have a class, or else kivy throws a fit
     pass
 
 class Player(Widget):
     health = NumericProperty(0)
 
-class PongGame(Widget):
-    secCounter = 0
-    player = ObjectProperty(None)
+    #for when he takes damage
+    #pain should be a number
+    def hurt(pain):
+        self.health -= pain
 
+class Game(Widget):
+
+    player = ObjectProperty(None)
+    
     def __init__(self, **kwargs):
-        super(PongGame, self).__init__(**kwargs)
+        super(Game, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self._keyboard.bind(on_key_up=self._on_keyboard_up)
+        self.add_widget(Obstacle(size = (10, 10))) #trial obstacle, I will formalize this code more later
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
@@ -37,25 +48,18 @@ class PongGame(Widget):
 
     def _on_keyboard_up(self, _keyboard, keycode):
         if keycode[1] == 'w':
-            # if(self.player.vel_y > 0):
-            #     self.player.vel_y -= 1
             self.player.vel_y = 0
         elif keycode[1] == 's':
-            # if(self.player.vel_y > 0):
-            #     self.player.vel_y += 1
             self.player.vel_y = 0
         elif keycode[1] == 'a':
-            # if(self.player.vel_x > 0):
-            #     self.player.vel_x += 1
             self.player.vel_x = 0
         elif keycode[1] == 'd':
-            # if(self.player.vel_x > 0):
-                # self.player.vel_x -= 1
             self.player.vel_x = 0
         return True
 
     def update(self, dt):
-        # secCounter = 0
+        
+        #no escaping!
         if self.player.x < 0:
             self.player.x = 0
             self.player.vel_x = 0
@@ -69,49 +73,34 @@ class PongGame(Widget):
             self.player.y = 0
             self.player.vel_y = 0
 
-
+        #so he can actually move
+        #velocity makes for a much smoother movement than just incrementing position
+        prev_x = self.player.center_x
+        prev_y = self.player.center_y
         self.player.center_x += self.player.vel_x
         self.player.center_y += self.player.vel_y
 
-        self.secCounter += 1
-        # if self.secCounter == 60:
-        #     if self.player.vel_x > 0:
-        #         self.player.vel_x -= .5
-        #     if self.player.vel_x < 0:
-        #         self.player.vel_x += .5
-        #     if self.player.vel_y > 0:
-        #         self.player.vel_y -= .5
-        #     if self.player.vel_y < 0:
-        #         self.player.vel_y += .5   
-        #     self.secCounter = 0 
-
-        # self.ball.move()
-        # self.player.x += 1
-        #bounce of paddles
-        # self.player1.bounce_ball(self.ball)
-        # self.player2.bounce_ball(self.ball)
-
-        #bounce ball off bottom or top
-        # if (self.ball.y < self.y) or (self.ball.top > self.top):
-        #     self.ball.velocity_y *= -1
-
-        # #went of to a side to score point?
-        # if self.ball.x < self.x:
-        #     self.player2.score += 1
-        #     self.serve_ball(vel=(4, 0))
-        # if self.ball.x > self.width:
-        #     self.player1.score += 1
-        #     self.serve_ball(vel=(-4, 0))
-
+        #check to see if it collides with any obstacles
+        for i in self.children:
+            try:
+                #test if its colliding
+                if self.player.collide_widget(i) and i.name == "obstacle":
+                    new_x = self.player.center_x #try resetting the x value
+                    self.player.center_x = prev_x
+                    if self.player.collide_widget(i) and i.name== "obstacle":
+                        self.player.center_y = prev_y #well, since it wasn't the x value, lets try y
+                        self.player.center_x = new_x
+                        if self.player.collide_widget(i) and i.name== "obstacle":
+                            self.player.center_x = prev_x #holy shit, still colliding? must be both then
+            except AttributeError:
+                pass
 
 
 class testApp(App):
     def build(self):
-        game = PongGame()
-        # game.serve_ball()
+        game = Game()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
-
-
+        
 if __name__ == '__main__':
     testApp().run()
