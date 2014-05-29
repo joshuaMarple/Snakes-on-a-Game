c69248b994c52001865061f20e8fa9e67bd95e3b
Joshua Marple <marpljos@gmail.com>
2014-05-28 22:18:55 -0400

    cleaned up update method

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
 
+#see this space here? see how there's no global state? keep it that way.
+#global functions are ok, so long as they don't add state
+
 
 class Baddie(Widget):
     name = "baddie"
@@ -21,6 +24,66 @@ class Player(Widget):
     baddieTimer = 0
     health = NumericProperty(0)
 
+    def update(self, widgets, parent_width, parent_height):
+
+         #no escaping!
+        if self.x < 0:
+            self.x = 0
+            self.vel_x = 0
+        if self.right > parent_width:
+            self.right = parent_width
+            self.vel_x = 0
+        if self.top > parent_height:
+            self.top = parent_height
+            self.vel_y = 0
+        if self.y < 0:
+            self.y = 0
+            self.vel_y = 0
+        #update the baddie timer
+        if self.baddieTimer > 0:
+            self.baddieTimer -= 1
+
+        #so he can actually move
+        #velocity makes for a much smoother movement than just incrementing position
+        prev_x = self.center_x
+        prev_y = self.center_y
+        self.center_x += self.vel_x
+        self.center_y += self.vel_y
+
+        for i in widgets:
+            try:
+                #test if its colliding
+                if self.collide_widget(i) and i.name == "obstacle":
+                    new_x = self.center_x #try resetting the x value
+                    self.center_x = prev_x
+                    if self.collide_widget(i) and i.name== "obstacle":
+                        self.center_y = prev_y #well, since it wasn't the x value, lets try y
+                        self.center_x = new_x
+                        if self.collide_widget(i) and i.name== "obstacle":
+                            self.center_x = prev_x #holy shit, still colliding? must be both then
+            except AttributeError:
+                pass
+
+        #check to see if player collides with baddies
+        for i in widgets:
+            try:
+                #test if its colliding
+                if self.collide_widget(i) and i.name == "baddie":
+                    if self.baddieTimer == 0: #prevents him from getting damaged so quick
+                        # print(self.source)
+                        self.source = 'media/p1_hurt.png'
+                        self.hurt(1)
+                        self.baddieTimer = 120
+                    new_x = self.center_x #try resetting the x value
+                    self.center_x = prev_x
+                    if self.collide_widget(i) and i.name== "baddie":
+                        self.center_y = prev_y #well, since it wasn't the x value, lets try y
+                        self.center_x = new_x
+                        if self.collide_widget(i) and i.name== "baddie":
+                            self.center_x = prev_x #holy shit, still colliding? must be both then
+            except AttributeError:
+                pass
+
     #for when he takes damage
     #pain should be a number
     def hurt(self, pain):
@@ -36,6 +99,7 @@ class Game(Widget):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self._keyboard.bind(on_key_up=self._on_keyboard_up)
         self.add_widget(Obstacle(size = (10, 10))) #trial obstacle, I will formalize this code more later
+
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
@@ -62,63 +126,8 @@ class Game(Widget):
         return True
 
     def update(self, dt):
-        #update the baddie timer
-        if self.player.baddieTimer > 0:
-            self.player.baddieTimer -= 1
-
-        #no escaping!
-        if self.player.x < 0:
-            self.player.x = 0
-            self.player.vel_x = 0
-        if self.player.right > self.parent.width:
-            self.player.right = self.parent.width
-            self.player.vel_x = 0
-        if self.player.top > self.parent.height:
-            self.player.top = self.parent.height
-            self.player.vel_y = 0
-        if self.player.y < 0:
-            self.player.y = 0
-            self.player.vel_y = 0
-
-        #so he can actually move
-        #velocity makes for a much smoother movement than just incrementing position
-        prev_x = self.player.center_x
-        prev_y = self.player.center_y
-        self.player.center_x += self.player.vel_x
-        self.player.center_y += self.player.vel_y
-
         #check to see if it collides with any obstacles
-        for i in self.children:
-            try:
-                #test if its colliding
-                if self.player.collide_widget(i) and i.name == "obstacle":
-                    new_x = self.player.center_x #try resetting the x value
-                    self.player.center_x = prev_x
-                    if self.player.collide_widget(i) and i.name== "obstacle":
-                        self.player.center_y = prev_y #well, since it wasn't the x value, lets try y
-                        self.player.center_x = new_x
-                        if self.player.collide_widget(i) and i.name== "obstacle":
-                            self.player.center_x = prev_x #holy shit, still colliding? must be both then
-            except AttributeError:
-                pass
-
-        #check to see if he collides with baddies
-        for i in self.children:
-            try:
-                #test if its colliding
-                if self.player.collide_widget(i) and i.name == "baddie":
-                    if self.player.baddieTimer == 0: #prevents him from getting damaged so quick
-                        self.player.hurt(1)
-                        self.player.baddieTimer = 120
-                    new_x = self.player.center_x #try resetting the x value
-                    self.player.center_x = prev_x
-                    if self.player.collide_widget(i) and i.name== "baddie":
-                        self.player.center_y = prev_y #well, since it wasn't the x value, lets try y
-                        self.player.center_x = new_x
-                        if self.player.collide_widget(i) and i.name== "baddie":
-                            self.player.center_x = prev_x #holy shit, still colliding? must be both then
-            except AttributeError:
-                pass
+        self.player.update(self.children, self.width, self.height)
 
 class testApp(App):
     def build(self):
