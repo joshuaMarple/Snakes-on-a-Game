e53870f8cfd49c41afea964b4ef0ebed2cf06f51
Joshua Marple <marpljos@gmail.com>
2014-06-01 09:05:48 -0400

    room calculator not working, pushing so marshall can work on it

--- dungeon.py
+++ dungeon.py
@@ -4,8 +4,6 @@ def generate():
     map = []
     width = randint(4,64)
     height = randint(4,64)
-#    width = 16
-#    height = 16
     min_room_size = 4
     max_room_size = width
     num_x_girders = randint(1,width/4) 
@@ -14,15 +12,19 @@ def generate():
     last_room_y = 0
     x_girders = [0, width-1]
     y_girders = [0, height-1]
+    rooms = []
+    for i in range(0, len(x_girders)):
+        room = []
+        for j in range(0, len(y_girders)):
+            room = [[x_girders[i-1], y_girders[j-1]],[x_girders[i]], [y_girders[j]]]
+        rooms.append(room)
     for i in range(0, num_x_girders):
-    # while len(x_girders) < num_x_girders:
         if last_room_x + min_room_size < width:
             x = randint(last_room_x + min_room_size, width)
             if x not in x_girders and width - x > min_room_size:
                 x_girders.append(x)
                 last_room_x = x
     for i in range(0, num_y_girders):
-    # while len(y_girders) < num_y_girders:
         if last_room_y + min_room_size < height:
             y = randint(last_room_y + min_room_size, height)
             if y not in y_girders and height - y > min_room_size:
@@ -36,6 +38,11 @@ def generate():
             else:
                 tmp_map.append("#")
         map.append(tmp_map)
+
+
+    # for i in map:
+    #     for j in i:
+
     print "num x girders: ", num_x_girders
     print "num y girders: " , num_y_girders
     print "x_girders: ", x_girders
@@ -44,4 +51,5 @@ def generate():
         for j in i:
             print(j),
         print
+    print rooms
 generate()
