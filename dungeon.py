ee187e48f0a3fe2a583a4be41d5c971c984848e9
Joshua Marple <marpljos@gmail.com>
2014-06-01 11:48:42 -0400

    modified the random number generators

--- dungeon.py
+++ dungeon.py
@@ -1,13 +1,14 @@
 from random import *
 from dungeon_room import *
+
 def generate():
     map = []
-    width = randint(4,64)
-    height = randint(4,64)
-    min_room_size = 4
-    max_room_size = width
-    num_x_girders = randint(1,width)
-    num_y_girders = randint(1, height) #divide by 4, as that is the min size of the room 
+    width = randint(32,64)
+    height = randint(32,64)
+    min_room_size = 6
+    max_room_size = 16
+    num_x_girders = randint(4,width)
+    num_y_girders = randint(4, height) #divide by 4, as that is the min size of the room 
     last_room_x = 0
     last_room_y = 0
     x_girders = [0, width-1]
@@ -18,7 +19,7 @@ def generate():
     #generate the columns of the walls
     for i in range(0, num_x_girders):
         if last_room_x + min_room_size < width:
-            x = randint(last_room_x + min_room_size, width)
+            x = randint(last_room_x + min_room_size, last_room_x + max_room_size)
             if x not in x_girders and width - x > min_room_size:
                 x_girders.append(x)
                 last_room_x = x
@@ -26,28 +27,34 @@ def generate():
     #generate the rows of the walls
     for i in range(0, num_y_girders):
         if last_room_y + min_room_size < height:
-            y = randint(last_room_y + min_room_size, height)
+            y = randint(last_room_y + min_room_size, last_room_y + max_room_size)
             if y not in y_girders and height - y > min_room_size:
                 y_girders.append(y)
                 last_room_y = y
 
     x_girders.sort()
     y_girders.sort()
-    # print(x_girders)
-    # print(y_girders)
 
     #calculate each of the rooms, make them an object
     for i in range(0, len(x_girders) - 1):
         room = []
         for j in range(0, len(y_girders) - 1):
-            # print "test", i, j
-            # print([x_girders[i],y_girders[j]])
             rooms.append(DungeonRoom(x_girders[i], y_girders[j], x_girders[i+1], y_girders[j+1]))
 
     #decide which rooms to fill in
     for i in rooms:
-        if random() < 0.5: #arbitrary, can be changed
+        if random() < .5: #arbitrary, can be changed
             i.fill = True
+
+    #takes care of the possibility of no rooms
+    allFull = True        
+    for i in rooms:
+        if i.fill == False:
+            allFull = False
+
+    if allFull == True:
+        rooms[0].fill = False
+
 
     #generate the map
     for i in range(0,height):
@@ -70,19 +77,10 @@ def generate():
     print("num y girders: " , num_y_girders)
     print("x_girders: ", x_girders)
     print("y_girders: ", y_girders)
-    y_pos = 0
-    # print
-    # for i in range(0, len(map[0])):
-        # print(i % 10),
-    # print
-    # print
 
     for i in map:
-        # print(y_pos % 10),
         for j in i:
             print(j),
         print
-        y_pos += 1
-    # for room in rooms:
-    #     print(room)
+
 generate()
--- dungeon_room.py
+++ dungeon_room.py
@@ -13,14 +13,6 @@ class DungeonRoom():
 		return "Upper Left: {} Lower Right: {} Center: {}".format(self.upperLeft, self.lowerRight, self.center)
 
 	def isIn(self, x, y):
-		# print self.upperLeft
-		# print self.lowerRight
-		# print self.upperLeft[0]
-		# print x
-		# print self.lowerRight[0]
-		# print self.upperLeft[1]
-		# print y
-		# print self.lowerRight[1]
 		if self.upperLeft[0] <= x <= self.lowerRight[0]:
 			if self.upperLeft[1] <= y <= self.lowerRight[1]: #since our coordinate system is flipped along the y axis
 				return True
